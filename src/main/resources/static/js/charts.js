a90ab59877175a8ef877abf48ce3e52790f8d2de
PaulaRuizRubio <paula.ruizrubio2003@gmail.com>
2025-02-06 23:35:18 +0100

    Modificaciones dashboard

---
+++ src/main/resources/static/js/charts.js
@@ -0,0 +1,31 @@
+// Esperar a que el DOM se cargue completamente antes de ejecutar los gráficos
+document.addEventListener("DOMContentLoaded", function () {
+    // Gráfico de barras
+    const revenueCtx = document.getElementById('revenueChart').getContext('2d');
+    new Chart(revenueCtx, {
+        type: 'bar',
+        data: {
+            labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+            datasets: [{
+                label: 'Cantidad de actividades realizadas durante el mes',
+                data: [10, 2, 30, 40, 5, 60, 1, 20, 3, 4, 50, 6],
+                backgroundColor: '#2c4a7f' // Color azul intermedio
+            }]
+        },
+        options: { responsive: true }
+    });
+
+    // Gráfico de pastel
+    const segmentsCtx = document.getElementById('segmentsChart').getContext('2d');
+    new Chart(segmentsCtx, {
+        type: 'pie',
+        data: {
+            labels: ['1', '2', '3', '4', '5'],
+            datasets: [{
+                data: [2, 5, 7, 1, 12],
+                backgroundColor: ['#e0f0ff', '#aad3ff', '#80b8ff', '#4f8cff', '#2c4a7f'],
+            }]
+        },
+        options: { responsive: true }
+    });
+});
