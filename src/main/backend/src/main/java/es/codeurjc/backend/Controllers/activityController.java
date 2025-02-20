832f2b407dfcb651b89979d79ac0a4db9d6aad5e
AlbitaVM <albavm2003@gmail.com>
2025-02-20 18:20:01 +0100

    mostrar todas las actividades

--- src/main/backend/src/main/java/es/codeurjc/backend/Controllers/activityController.java
+++ src/main/backend/src/main/java/es/codeurjc/backend/Controllers/activityController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import es.codeurjc.backend.Model.Activity;
 import es.codeurjc.backend.Service.ActivityService;
+import es.codeurjc.backend.Service.UserService;
+
+
+
 
 @Controller
 public class activityController {
@@ -16,11 +20,23 @@ public class activityController {
     @Autowired
     private ActivityService activityService;
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/")
     public String showActivities(Model model) {
         List<Activity> activities = activityService.findAll();
         model.addAttribute("activities", activities);
-        return "index"; // Nombre del HTML en templates/
+        return "index"; 
     }
+
+    @GetMapping("/admin_activities")
+    public String showAdminActivities(Model model) {
+        model.addAttribute("allActivities", activityService.findAll());
+        model.addAttribute("activityCount", activityService.activityCount());
+        model.addAttribute("userCount", userService.countUsers());
+        return "admin_activities";
+    }
+    
 }
 
