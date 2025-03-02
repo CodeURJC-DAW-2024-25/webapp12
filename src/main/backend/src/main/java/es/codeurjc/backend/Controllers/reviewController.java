752938242eeb03151a68c062ef21c0738a2e4fc9
gonzaloperz <g.perezr.2019@alumnos.urjc.es>
2025-03-02 17:07:11 +0100

    Arreglado las reviews

--- src/main/backend/src/main/java/es/codeurjc/backend/Controllers/reviewController.java
+++ src/main/backend/src/main/java/es/codeurjc/backend/Controllers/reviewController.java
@@ -1,25 +1,43 @@
 package es.codeurjc.backend.Controllers;
 
+
+
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import es.codeurjc.backend.Model.User;
+import es.codeurjc.backend.Repository.UserRepository;
 import es.codeurjc.backend.Service.ReviewService;
 
-@RequestMapping("/activity")
+@Controller
 public class reviewController {
     
     @Autowired
     private ReviewService reviewService;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/activity/{activityId}/addReview")
     public String addReview(@PathVariable Long activityId, 
                             @RequestParam int starsValue, 
                             @RequestParam String description,
-                            @RequestParam Long userId
-                            ) {
+                            @RequestParam (required = false)Long userId,
+                            Principal principal
+                            ) 
+        {
+        String username = principal.getName();
+        User user = userRepository.findByEmail(username);
+        if (userId == null) {
+            userId = user.getId();
+        }
+        
         try {
             reviewService.saveReview(activityId, starsValue, description, userId);
             return "redirect:/activity/" + activityId;  // Redirigir a la página de actividad
